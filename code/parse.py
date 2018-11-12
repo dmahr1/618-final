08a17cbcea45ef336ecc143594daff96ca6477a3
Dan Mahr <danmahr23@gmail.com>
2018-11-12 16:01:58 -0500

    Add parser and runner scripts

---
+++ code/parse.py
@@ -0,0 +1,24 @@
+import sys, os
+
+current_line = []
+
+for line in sys.stdin:
+
+    split = line.split()
+
+    if line.startswith("Input file"):
+
+        # Print previous
+        if current_line:
+            print("\t".join(current_line))
+            current_line = []
+
+        current_line.append(os.path.basename(split[3][:-1]))    # Filename
+        current_line.append(split[-5][:-1])                     # Number of threads
+        current_line.append(split[-1])                          # Number of blocks
+
+    elif split[-1] == "microseconds":
+        current_line.append(split[-2])
+
+if current_line:
+    print("\t".join(current_line))
